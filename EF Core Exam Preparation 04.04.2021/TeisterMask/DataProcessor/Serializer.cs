7a0c901288e50f4da4abaf5b23c994e98f83252b
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2024-01-03 22:37:02 +0200

    Update Serializer.cs

--- EF Core Exam Preparation 04.04.2021/TeisterMask/DataProcessor/Serializer.cs
+++ EF Core Exam Preparation 04.04.2021/TeisterMask/DataProcessor/Serializer.cs
@@ -13,19 +13,25 @@
     {
         public static string ExportProjectWithTheirTasks(TeisterMaskContext context)
         {
+            //using Data Transfer Object Class to map it with projects
             XmlSerializer serializer = new XmlSerializer(typeof(ExportProjectsDTO[]), new XmlRootAttribute("Projects"));
 
+            //using StringBuilder to gather all info in one string
             StringBuilder sb = new StringBuilder();
 
+            //"using" automatically closes opened connections
             using var writer = new StringWriter(sb);
 
             var xns = new XmlSerializerNamespaces();
+
+            //one way to display empty namespace in resulted file
             xns.Add(string.Empty, string.Empty);
 
             var projectsAndTasks = context.Projects
                 .Where(p => p.Tasks.Any())
                 .Select(p => new ExportProjectsDTO
                 {
+                    //using identical properties in order to map successfully
                     TasksCount = p.Tasks.Count,
                     ProjectName = p.Name,
                     HasEndDate = p.DueDate != null ? "Yes" : "No",
@@ -42,14 +48,20 @@
                 .ThenBy(p => p.ProjectName)
                 .ToArray();
 
+            //Serialize method needs file, TextReader object and namespace to convert/map
             serializer.Serialize(writer, projectsAndTasks, xns);
+
+            //explicitly closing connection in terms of reaching edge cases
             writer.Close();
 
-            return sb.ToString();
+            //using TrimEnd() to get rid of white spaces
+            return sb.ToString().TrimEnd();
         }
 
         public static string ExportMostBusiestEmployees(TeisterMaskContext context, DateTime date)
         {
+            //turning needed info about employees into a collection using anonymous object
+            //using less data
             var employeesAndTasks = context.Employees
                 .Where(s => s.EmployeesTasks.Any(bgs => bgs.Task.OpenDate >= date))
                 .OrderByDescending(s => s.EmployeesTasks.Count)
@@ -64,8 +76,8 @@
                     .Select(bg => new
                     {
                         TaskName = bg.Task.Name,
-                        OpenDate = bg.Task.OpenDate.ToString("d", CultureInfo.InvariantCulture),
-                        DueDate = bg.Task.DueDate.ToString("d", CultureInfo.InvariantCulture),
+                        OpenDate = bg.Task.OpenDate.ToString("d", CultureInfo.InvariantCulture), //using culture-independent format
+                        DueDate = bg.Task.DueDate.ToString("d", CultureInfo.InvariantCulture), //using culture-independent format
                         LabelType = bg.Task.LabelType.ToString(),
                         ExecutionType = bg.Task.ExecutionType.ToString(),
                     })
@@ -74,6 +86,8 @@
                 .Take(10)
                 .ToArray();
 
+            //Serialize method needs object to convert/map
+	        //adding Formatting for better reading 
             return JsonConvert.SerializeObject(employeesAndTasks, Formatting.Indented);
         }
     }
